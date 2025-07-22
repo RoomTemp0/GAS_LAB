df5508a12939ede6036f06679aa8529070070886
Zachary <zach.aryjacob27@gmail.com>
2025-07-22 18:21:00 -0400

    Update Spectro_Gas.py

--- Spectro_Gas.py
+++ Spectro_Gas.py
@@ -2,13 +2,10 @@
 # This code will include the threshold limit to begin scanning as well as the characater pointers that the spectrometer can read from its manual
 # This protocal will require an ethernet connection to a computer and a USB 2.0 connection to the spectrometer. 
 # The Pi can make the spectrometer do its commands using the charcter pointers for a scan, save the scan as a .csv file to the Pi, and then send it to the computer using communication protocol to send the files to the computer 
-import csv
-import pyusb
-import datetime
-import os
-import pandas
-import paramiko # This is to send the files via SCP/SSH protocal
-import socket # This is to send the files via TCP protocal
+import gpiozero
+import time
+import serial
+import RPi.GPIO as GPIO# This is to send the files via TCP protocal
 
 
 
